ccfa6a7366ec93aa892ef74f2960be7d163f020e
Pooja-27 <poojarm2797@gmail.com>
2026-07-11 23:55:31 +0530

    Level-2 problems

--- Conditional-Statements/Level2.java
+++ Conditional-Statements/Level2.java
@@ -4,19 +4,36 @@ public class Level2 {
         Scanner sc = new Scanner(System.in);
 
         //Power of 2
-        int n = sc.nextInt();
+        // int n = sc.nextInt();
 
-        if (n <= 0) System.out.println("No");
-        else {
-            while (n % 2 == 0) {
-                System.out.println(n);
-                n /= 2;
-            }
-            if (n == 1) System.out.println("Yes");
-            else System.out.println("No");
-        }
+        // if (n <= 0) System.out.println("No");
+        // else {
+        //     while (n % 2 == 0) {
+        //         System.out.println(n);
+        //         n /= 2;
+        //     }
+        //     if (n == 1) System.out.println("Yes");
+        //     else System.out.println("No");
+        // }
         
 
-        //
+        //Happy Number
+        int num = sc.nextInt();
+        int sum = 0;
+
+        while(num > 0) {
+            int digit = num % 10;
+            int square = digit * digit;
+            sum = sum + square;
+            num = num / 10;
+            if(sum == 1) break;
+            if(num == 0) {
+                num = sum;
+                sum = 0;
+            }
+        }
+        System.out.println(sum);
+        // if(sum == 1) System.out.println("Happy");
+        // else System.out.println("Not happy");
     }
 }
