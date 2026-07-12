30621407d1687cba1ee79b9f7f97b52d8234c4ee
Pooja-27 <poojarm2797@gmail.com>
2026-07-13 03:52:47 +0530

    Level-3 Started

---
+++ Conditional-Statements/Level3.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public interface Level3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Odd count in range
+        // int start = sc.nextInt();
+        // int end = sc.nextInt();
+        // int count = 0;
+
+        // for(int i=start; i <= end; i++) {
+        //     if(i % 2 != 0) count++;
+        // }
+        // System.out.println(count);
+
+
+        //Count operations
+        // int num1 = sc.nextInt();
+        // int num2 = sc.nextInt();
+        // int count = 0;
+
+        // if(num1 == 0 || num2 == 0) System.out.println(count);
+        // else if(num1 == num2) System.out.println(1);
+        // else {
+        //     while(num1 > 0 && num2 > 0) {
+        //         if(num1 > num2) num1 -= num2;
+        //         else num2 -= num1;
+        //         count++;
+        //     }
+        //     System.out.println(count);
+        // }
+    }
+}
