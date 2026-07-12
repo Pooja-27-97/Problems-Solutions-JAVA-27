ce547ce5af5840c97f3d06d9856e633913d3574d
Pooja-27 <poojarm2797@gmail.com>
2026-07-12 21:55:46 +0530

    Solved 2 problems

--- Conditional-Statements/Level2.java
+++ Conditional-Statements/Level2.java
@@ -11,6 +11,20 @@ public class Level2 {
         }
         return sum;
     }
+
+    //Roman to Integer
+    public static int value(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -64,8 +78,32 @@ public class Level2 {
 
 
         //Roman to Integer
-        int I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000;
-        String roman = sc.nextLine();
+        // String roman = sc.nextLine();
+        // int sum = 0;
         
+        // for(int i=0; i < roman.length(); i++) {
+        //     int curr = value(roman.charAt(i));
+        //     if(i < roman.length() - 1) {
+        //         int next = value(roman.charAt(i + 1));
+        //         if(curr < next) sum -= curr;
+        //         else sum += curr;
+        //     } else {
+        //         sum += curr;
+        //     }
+        // }
+        // System.out.println("Value: " + sum);
+
+
+        //Grading Students
+        int n = sc.nextInt();
+        for(int i=0; i < n; i++) {
+            int grade = sc.nextInt();
+            int diff = 5 - (grade % 5);
+
+            if(grade >= 0 && grade <= 100) {
+                if (grade >= 38 && diff < 3) System.out.println(grade + diff);
+                else System.out.println(grade);
+            } else System.out.println("Out of range");
+        }
     }
 }
