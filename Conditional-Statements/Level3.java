041f93d83bfae6c814a1ce59c69eecb9addc1353
Pooja-27 <poojarm2797@gmail.com>
2026-07-16 01:25:02 +0530

    Java Datatypes

--- Conditional-Statements/Level3.java
+++ Conditional-Statements/Level3.java
@@ -29,5 +29,38 @@ public interface Level3 {
         //     }
         //     System.out.println(count);
         // }
+
+
+        //Java Datatypes
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+
+            try {
+
+                long num = sc.nextLong();
+
+                System.out.println(num + " can be fitted in:");
+
+                if (num >= Byte.MIN_VALUE && num <= Byte.MAX_VALUE)
+                    System.out.println("* byte");
+
+                if (num >= Short.MIN_VALUE && num <= Short.MAX_VALUE)
+                    System.out.println("* short");
+
+                if (num >= Integer.MIN_VALUE && num <= Integer.MAX_VALUE)
+                    System.out.println("* int");
+
+                if (num >= Long.MIN_VALUE && num <= Long.MAX_VALUE)
+                    System.out.println("* long");
+
+            } catch (Exception e) {
+
+                String num = sc.next();
+
+                System.out.println(num + " can't be fitted anywhere.");
+            }
+        }
+
     }
 }
