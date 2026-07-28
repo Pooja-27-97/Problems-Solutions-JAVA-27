f39dd49ba10f8a7579f36013113a338a7c5ab9f0
Pooja-27 <poojarm2797@gmail.com>
2026-07-28 21:08:03 +0530

    Variables and Data Types

---
+++ Week-1/Basics.java
@@ -0,0 +1,9 @@
+public class Basics {
+    public static void main(String[] args) {
+        int bi = 0b1111;
+        int hex = 0x1F;
+
+        System.out.println(bi);
+        System.out.println(hex);
+    }
+}
