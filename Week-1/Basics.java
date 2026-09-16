2e9e18ad836020d8f1a83332860af238ed6aa169
Pooja-27 <poojarm2797@gmail.com>
2026-09-17 00:27:24 +0530

    Deleted some files

--- Week-1/Basics.java
+++
@@ -1,9 +0,0 @@
-public class Basics {
-    public static void main(String[] args) {
-        int bi = 0b1111;
-        int hex = 0x1F;
-
-        System.out.println(bi);
-        System.out.println(hex);
-    }
-}
