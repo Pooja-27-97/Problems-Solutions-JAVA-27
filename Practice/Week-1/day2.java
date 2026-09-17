34dcfe4fc92a423bf62182f4410fb5e918401f6e
Pooja-27 <poojarm2797@gmail.com>
2026-09-17 23:32:41 +0530

    6-10 problems

---
+++ Practice/Week-1/day2.java
@@ -0,0 +1,64 @@
+import java.util.*;
+public class day2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Problem-6
+        // int n = sc.nextInt();
+        // int temp = n;
+        // int place = 1;
+        // int ninePlace = 0;
+
+        // while (temp > 0) {
+        //     int lastDigit = temp % 10;
+        //     if(lastDigit == 9) ninePlace = place;
+        //     temp /= 10;
+        //     place *= 10;
+        // }
+
+        // System.out.println("Min: " + (n - 3 * ninePlace));
+
+
+        //Problem-7
+        // int num1 = sc.nextInt();
+        // int num2 = sc.nextInt();
+        // int sum = num1 + num2;
+        // System.out.println("Sum: " + sum);
+
+
+        //Problem-8
+        // int a = 0 , b = 0;
+        // int a1 = sc.nextInt();
+        // int a2 = sc.nextInt();
+        // int a3 = sc.nextInt();
+
+        // int b1 = sc.nextInt();
+        // int b2 = sc.nextInt();
+        // int b3 = sc.nextInt();
+
+        // if(a1 > b1) a++;
+        // else b++;
+
+        // if(a2 > b2) a++;
+        // else b++;
+
+        // if(a3 > b3) a++;
+        // else b++;
+
+        // System.out.println("A: " + a + " B: " + b);
+
+
+        //Problem-9
+        // int num = sc.nextInt();
+        // if(num % 2 == 0) System.out.println("Even");
+        // else System.out.println("Odd");
+
+
+        //Problem-10
+        // int num = sc.nextInt();
+
+        // if(num > 0) System.out.println("Positive");
+        // else if(num < 0) System.out.println("Negative");
+        // else System.out.println("Zero");
+    }
+}
