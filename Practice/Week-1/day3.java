c9ac5bea10888e05ef87ee073ed6781890089fc7
Pooja-27 <poojarm2797@gmail.com>
2026-09-19 01:20:08 +0530

    Solved 4 problems

---
+++ Practice/Week-1/day3.java
@@ -0,0 +1,56 @@
+import java.util.*;
+public class day3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Problem-11
+        // int num1 = sc.nextInt();
+        // int num2 = sc.nextInt();
+
+        // System.out.println(num1 + " " + num2);
+
+        // // int temp = num1;
+        // // num1 = num2;
+        // // num2 = temp;
+
+        // num1 = num1 + num2;
+        // num2 = num1 - num2;
+        // num1 = num1 - num2;
+
+        // System.out.println(num1 + " " + num2);
+
+
+        //Problem-12
+        // double radius = sc.nextDouble();
+        // double pi = 3.14;
+
+        // System.out.println("Area of circle: " + (pi * radius * radius));
+
+
+        //Problem-13
+        // int year = sc.nextInt();
+
+        // if(year % 4 == 0) {
+        //     if(year % 100 == 0) {
+        //         if(year % 400 == 0) System.out.println("Leap year");
+        //         else System.out.println("Not a Leap year");
+        //     } else System.out.println("Leap year");
+        // } else System.out.println("Not a Leap year");
+
+
+        //Problem-14
+        // int num = sc.nextInt();
+
+        // if(num <= 0) System.out.println("No");
+        // else {
+        //     while (num % 2 == 0) {
+        //         num = num / 2;
+        //     }
+        //     if(num == 1) System.out.println("Yes!");
+        //     else System.out.println("No");
+        // }
+
+
+        //Problem-15
+    }
+}
